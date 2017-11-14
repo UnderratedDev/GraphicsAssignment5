c0d8a77ad83fb5fe8988d7bebb66e558ed701781
Underrateddev <rajyudhvir29@gmail.com>
2017-11-13 20:39:39 -0800

    Adding a matrix works, and initalising them

---
+++ asgn5student/MatrixLibrary/Matrix.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asgn5v1.MatrixLibrary
+{
+    class Matrix
+    {
+        private int x_len;
+        private int y_len;
+        private double[,] matrix;
+
+        public Matrix (int x, int y)
+        {
+            this.x_len = x;
+            this.y_len = y;
+            this.matrix = new double[x_len, y_len];
+        }
+
+        public void setXLen (int x)
+        {
+            this.x_len = x;
+            this.matrix = new double[x_len, y_len];
+        }
+        
+        public void setYLen (int y)
+        {
+            this.y_len = y;
+            this.matrix = new double[x_len, y_len];
+        }
+
+        public void setMatrix(double[,] matrix, int x, int y)
+        {
+            this.matrix = matrix;
+            x_len = x;
+            y_len = y;
+        }
+
+        public int getXLen ()
+        {
+            return x_len;
+        }
+
+        public int getYLen()
+        {
+            return y_len;
+        }
+
+        public double[,] getMatrix()
+        {
+            return this.matrix;
+        }
+
+        public void insertValue (int x, int y, double value)
+        {
+            this.matrix[x, y] = value;
+        }
+
+        public double getValue (int x, int y)
+        {
+            return this.matrix[x, y];
+        }
+
+        public void printMatrix ()
+        {
+            for (int y = 0; y < y_len; ++y)
+            {
+                Console.Write("[");
+                for (int x = 0; x < x_len; ++x)
+                {
+                    Console.Write(this.matrix[x, y] + " ");
+                }
+                Console.WriteLine("]");
+            }
+           
+        }
+
+
+    }
+}
---
+++ asgn5student/MatrixLibrary/MatrixManipulation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace asgn5v1.MatrixLibrary
+{
+    class MatrixManipulation
+    {
+        public static Matrix addMatricies (Matrix a, Matrix b)
+        {
+            if (a == null || b == null || a.getXLen() != b.getXLen() || a.getYLen() != b.getYLen ())
+            {
+                return null;
+            }
+
+            int width  = a.getXLen();
+            int height = b.getYLen();
+
+            Matrix c = new Matrix(width, height);
+
+            double value = 0;
+
+            for (int x = 0; x < width; ++x)
+            {
+                for (int y = 0; y < height; ++y)
+                {
+                    value = a.getValue(x, y) + b.getValue(x, y);
+                    c.insertValue(x, y, value);
+                }
+            }
+
+            return c;
+        }
+
+        public static void multiplyMatrix (ref Matrix m, double multiplier)
+        {
+
+        }
+    }
+}
